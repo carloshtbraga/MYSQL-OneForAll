5a78dbc22205676e82ea0f6a96a932dca8d333fd
carloshtbraga <carloshtbraga@gmail.com>
2023-04-05 00:41:02 -0300

    Requisito 2
    Crie uma QUERY que exiba três colunas:
    
    A primeira coluna deve exibir a quantidade total de canções. Dê a essa coluna o alias "cancoes".
    
    A segunda coluna deve exibir a quantidade total de artistas e deverá ter o alias "artistas".
    
    A terceira coluna deve exibir a quantidade de álbuns e deverá ter o alias "albuns".

---
+++ challenges/desafio2.sql
@@ -0,0 +1,7 @@
+SELECT
+    COUNT(m.musica_id) AS cancoes, 
+    COUNT(DISTINCT ar.artista_id) AS artistas,
+    COUNT( DISTINCT al.album_id) as albuns 
+FROM musicas AS m
+JOIN albuns AS al ON al.album_id = m.album_id
+JOIN artistas AS ar ON ar.artista_id = al.artista_id
